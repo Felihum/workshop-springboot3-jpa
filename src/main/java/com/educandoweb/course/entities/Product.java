015eaaeb9b18665d8b1bde43edbbdcf9cd1cbcd0
Felihum <121069824+Felihum@users.noreply.github.com>
2023-12-26 12:59:37 -0300

    Product Collections and Categotries Collections

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -1,12 +1,15 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
 @Table(name="tb_product")
@@ -21,6 +24,9 @@ public class Product implements Serializable{
 	private String description;
 	private Double price;
 	private String imgUrl;
+	
+	@Transient
+	private Set<Category> categories = new HashSet<>();
 	
 	public Product() {
 		
@@ -97,5 +103,9 @@ public class Product implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
 	}
 }
